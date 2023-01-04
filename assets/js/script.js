6ac6cf4177c89c8bdb787d58e153a0a68d0d0d6b
michael seaman <mseaman26@gmail.com>
2023-01-04 00:27:03 -0800

    styling and elements.  Starting JS logic

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var words = ['boolean', 'doccument', 'variable', 'function']
+function playGame(){
+    var currentWord = Math.floor(Matth)
+}
+
+document.querySelector('#start_button').addEventListener('click', playGame)
